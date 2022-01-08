02eaca6a2edd1336ba4f5e6d295c4b07b5e33a70
doky <48897815+iamdoky@users.noreply.github.com>
2022-01-08 23:26:17 +0900

    2022.01.08 : 11) 게시글삭제까지

--- src/main/java/com/study/baord/service/BoardService.java
+++ src/main/java/com/study/baord/service/BoardService.java
@@ -13,11 +13,21 @@ public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
 
+    // 게시글 등록
     public void boardWrite(Board board) {
         boardRepository.save(board);
     }
 
+    // 게시글 목록
     public List<Board> boardList() {
         return boardRepository.findAll();
     }
+
+    // 게시글 상세
+    public Board boardView (Integer id) { return boardRepository.findById(id).get(); }
+
+    // 게시글 삭제
+    public void boardDelete(Integer id) { boardRepository.deleteById(id); }
+
+
 }
