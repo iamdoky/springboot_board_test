f67ab530b576e803ee62eb433bb85653b1b01411
iamdoky <kdh6631@gmail.com>
2022-01-10 14:32:28 +0900

    2022.01.10 : 게시판 따라하기 완료.!
    DB -> filename, filepath 추가
    resouces.static.files -> 디렉토리 추가

--- src/main/java/com/study/baord/controller/BoardController.java
+++ src/main/java/com/study/baord/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class BoardController {
@@ -23,50 +24,54 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board) {
+    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception {
 
-        boardService.boardWrite(board);
-        return "redirect:/board/list";
+        boardService.boardWrite(board, file);
+
+        model.addAttribute("message","글 작성이 완료 되었습니다.");
+        model.addAttribute("searchUrl","/board/list");
+
+        return "message";
     }
 
     @GetMapping("/board/list")
-    public String boardList(Model model) {
+    public String boardList( Model model ) {
 
         model.addAttribute("list", boardService.boardList());
         return "boardList";
     }
 
     @GetMapping("/board/view")  // localhost:8080/board/view?id=1 -> get 방식의 parameter
-    public String boardView(Model model, Integer id) {
+    public String boardView( Model model, Integer id ) {
 
-        model.addAttribute("board", boardService.boardView(id));
+        model.addAttribute("board", boardService.boardView(id) );
         return "boardView";
     }
 
     @GetMapping("/board/delete")
-    public String boardDelete(Integer id) {
+    public String boardDelete( Integer id ) {
         System.out.println("######" + id);
         boardService.boardDelete(id);
         return "redirect:/board/list";
     }
 
     @GetMapping("/board/modify/{id}")
-    public String boardModify(@PathVariable("id") Integer id, Model model) {
+    public String boardModify( @PathVariable("id") Integer id, Model model ) {
         model.addAttribute("board", boardService.boardView(id));
         return "boardModify";
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board) {
+    public String boardUpdate( @PathVariable("id") Integer id, Board board, MultipartFile file ) throws Exception {
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
 
         System.out.println("#########################################");
-        System.out.println(boardTemp.getTitle());
-        System.out.println(boardTemp.getContent());
+        System.out.println( boardTemp.getTitle() );
+        System.out.println( boardTemp.getContent() );
 
-        boardService.boardWrite(boardTemp);
+        boardService.boardWrite(boardTemp, file);
 
         return "redirect:/board/list";
     }
